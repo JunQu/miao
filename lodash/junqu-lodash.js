27bbe044fba3e096c4fe6feab27e512b46d7cdaf
JunQu <junjv@outlook.com>
2019-01-10 11:33:52 +0800

    test  lodash

---
+++ lodash/junqu-lodash.js
@@ -0,0 +1,18 @@
+var USERNAME = function(){
+
+  function chunk(ary, size = 1) {
+    if (size < 1) return array;
+    let result = [[]];
+    for (let i = 0; i < array.length; i++) {
+        if (result[result.length - 1].length >= size){ // 假如已经填满了，就往后移动
+            result.push([]);
+        }
+        result[result.length - 1].push(array[i]); // 进行元素分割
+    }
+    return result;
+  }
+
+  return {
+    chunk: chunk
+  }
+}()
