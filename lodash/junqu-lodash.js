dcf98fd231a7876a8edcc9d82799add796f064b5
JunQu <junjv@outlook.com>
2019-01-27 21:24:04 +0800

    change lodash style

--- lodash/junqu-lodash.js
+++ lodash/junqu-lodash.js
@@ -1,29 +1,22 @@
-var junqu = function(){
-//array
-  function chunk(array, size = 1) {
-    if (size < 1) return array;
-    let result = [[]];
-    for (let i = 0; i < array.length; i++) {
-        if (result[result.length - 1].length >= size){ // 假如已经填满了，就往后移动
-            result.push([]);
+var junqu = {
+    chunk: function(array, size = 1) {
+        if (size < 1) return array;
+        let result = [[]];
+        for (let i = 0; i < array.length; i++) {
+            if (result[result.length - 1].length >= size){ // 假如已经填满了，就往后移动
+                result.push([]);
+            }
+            result[result.length - 1].push(array[i]); // 进行元素分割
         }
-        result[result.length - 1].push(array[i]); // 进行元素分割
-    }
-    return result;
-  }
-
-  const compact = function(array){
-    let result = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i]){
-             result.push(array[i]);
+        return result;
+    },
+    compact: function(array){
+        let result = [];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i]){
+                result.push(array[i]);
+            }
         }
-    }
-    return result;
-  }
-  
-  return {
-    chunk: chunk,
-    compact: compact,
-  }
-}()
+        return result;
+    },
+};
